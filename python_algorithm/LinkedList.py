1f3dc157ff4c25867c1d40bc86e0992208e0bde8
SnowGushiGit <snow.akogi.pgel@gmail.com>
2017-07-27 06:44:32 +0900

    Update how to search node

--- python_algorithm/LinkedList.py
+++ python_algorithm/LinkedList.py
@@ -30,7 +30,17 @@ def deleteDups(node: LinkedList=None):
             node_previous = node
         node = node.next
 
-if __name__ == "__main__":
+def deleteDupsNonbuffer(node: LinkedList=None):
+    while(node):
+        runner = node
+        while(runner.next):
+            if runner.next.current == node.current:
+                runner.next = runner.next.next
+            else:
+                runner = runner.next
+        node = node.next
+
+def make_linked_list():
     linkedlist = LinkedList(current_node='first')
     linkedlist2 = LinkedList(current_node='second')
     linkedlist3 = LinkedList(current_node='third')
@@ -39,8 +49,31 @@ if __name__ == "__main__":
     linkedlist.next = linkedlist2
     linkedlist2.next = linkedlist3
     linkedlist3.next = linkedlist4
+    return  linkedlist
 
+def printKthToLast(head: LinkedList=None, k: int=0):
+    if head is None:
+        return 0
+    index = printKthToLast(head.next, k) + 1
+    if index == k:
+        print("{0} th to last node is {1}".format(k, head.current))
+    return index
+
+if __name__ == "__main__":
+
+    linkedlist = make_linked_list()
     print_list(linkedlist)
     deleteDups(linkedlist)
     print('-----Delete Duplicate----')
     print_list(linkedlist)
+
+    print('-----Retry----')
+    linkedlist = make_linked_list()
+    deleteDupsNonbuffer(linkedlist)
+    print('-----Delete Duplicate Non buffer----')
+    print_list(linkedlist)
+
+    print('-----Linked list last node----')
+    linkedlist = make_linked_list()
+    printKthToLast(linkedlist, 2)
+
